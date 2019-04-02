256f7ade37a8c73b0e2dfe0a4d814cd7bea7b930
kanatunta <kanatunta@gmail.com>
2019-04-02 21:14:46 +0300

    add example 2

--- week4/lectOne_02_04_2019/javascript/main.js
+++ week4/lectOne_02_04_2019/javascript/main.js
@@ -1,3 +1,13 @@
 const interAct = () => {
   alert(event.target.innerHTML);
 };
+
+const arr = [1, 2, 3, 4, 5, 6];
+
+for (const iterator of arr) {
+  console.log(iterator);
+}
+
+const timesTen = arr.map(e=>e*10);
+
+console.log(timesTen);
